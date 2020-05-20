cc2fe80ad8886dce50934044edeaa19701a8fca1
salaboy <salaboy@gmail.com>
2020-05-20 16:44:20 +0100

    adding operator 73

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 base_url="http://chartmuseum.jx.35.233.51.246.nip.io"
-charts='["zeebe-cluster", "zeebe-operate", "zeebe-full", "zeeqs", "zeebe-cloud-events-router", "tickets-service", "payments-service"]'
+charts='["zeebe-cluster", "zeebe-operate", "zeebe-full", "zeebe-operator",  "zeeqs", "zeebe-cloud-events-router", "tickets-service", "payments-service"]'
 
 echo $charts | jq ".[]" | xargs -I ^ curl -s "$base_url/api/charts/^/" | jq '.urls[0]' | xargs -I ^ curl -Os "$base_url/^"
 
