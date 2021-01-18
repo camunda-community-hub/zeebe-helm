6990d147c1ea3a4d7fbf7b97925e48eb0a85b6a4
salaboy <salaboy@gmail.com>
2021-01-18 13:06:39 +0000

    adding updated release script

--- release.sh
+++ release.sh
@@ -4,4 +4,51 @@ charts='["zeebe-cluster-helm", "zeebe-operate-helm", "zeebe-full-helm", "zeebe-o
 
 echo $charts | jq ".[]" | xargs -I ^ curl -s "$base_url/api/charts/^/" | jq '.urls[0]' | xargs -I ^ curl -Os "$base_url/^"
 
+#! /bin/bash
+
+echo "Cleaing up release.yaml files from releases"
+workDir="tmp"
+
+if [ -d "$workDir" ]; then
+  echo "Deleting working dir"
+  rm -rf $workDir
+  echo "Done"
+  echo ""
+fi
+
+echo "Creating fresh working dir"
+mkdir $workDir
+echo "Done"
+echo ""
+
+files=`git status --porcelain | grep -v "^\sD" | grep ".*\.tgz$" | awk '{ if(system("tar -tf " $2 " | grep -c release.yaml > /dev/null") == 0 ) {print $2}}'`
+
+for tarFile in ${files}
+do
+
+  echo "Decompressing ${tarFile}"
+  tar xf ${tarFile} -C ${workDir}
+  echo "Done"
+  echo ""
+
+  echo "Files to be deleted:"
+  find ${workDir} -name "release.yaml" -delete -print
+  echo "Done"
+  echo ""
+
+  echo "Compressing new File"
+  cd ${workDir}
+  tar -czf ../${tarFile} .
+  rm -rf . > /dev/null 2>&1
+  cd - > /dev/null
+  echo "Done"
+  echo ""
+
+done
+
+echo "Cleaning up working dir"
+rm -rf ${workDir}
+echo "Done"
+echo ""
+
 helm repo index --url http://helm.zeebe.io .
