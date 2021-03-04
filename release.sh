fba51aa1180d3be457ae17b1cfb752dd76da01f5
salaboy <salaboy@gmail.com>
2021-03-04 10:12:21 -0300

    release full 133 and operate 42

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
-base_url="http://chartmuseum-jx.35.222.17.41.nip.io"
-charts='["zeebe-cluster-helm", "zeebe-operate-helm", "zeebe-full-helm", "zeebe-operator",  "zeebe-zeeqs-helm","zeebe-tasklist-helm", "zeebe-cloud-events-router", "tickets-service", "payments-service"]'
+base_url="http://chartmuseum-jx.34.67.22.199.nip.io"
+charts='["zeebe-cluster-helm", "zeebe-operate-helm", "zeebe-full-helm", "zeebe-operator","zeebe-operator-cc", "zeebe-zeeqs-helm","zeebe-tasklist-helm", "zeebe-cloud-events-router", "tickets-service", "payments-service"]'
 
 echo $charts | jq ".[]" | xargs -I ^ curl -s "$base_url/api/charts/^/" | jq '.urls[0]' | xargs -I ^ curl -Os "$base_url/^"
 
